011719d8091c53e0fc11143f8a3501f42364330c
Lorenz Schori <lo@znerol.ch>
2010-09-09 20:02:03 +0200

    Fix waiting condition in NSOperationQueue+NonBlockingWaitUntilFinished

--- src/NSOperationQueue+NonBlockingWaitUntilFinished.m
+++ src/NSOperationQueue+NonBlockingWaitUntilFinished.m
@@ -10,10 +10,25 @@
 
 
 @implementation NSOperationQueue (NonBlockingWaitUntilFinished)
+/**
+ *
+ *  Wait for until all operations in the queue have finished ore have been cancelled.
+ *  
+ *  isReady isExecuting isFinished isCanceled Action
+ *    0         0           0         0       wait
+ *    0         0           0         1       don't wait
+ *    0         0           1         0       don't wait
+ *    0         0           1         1       don't wait
+ *    0         1           0         0       wait
+ *    0         1           0         1       wait
+ *    1         0           0         0       wait
+ *    1         0           0         1       don't wait
+ */
 -(void)waitUntilAllOperationsAreFinishedPollingRunLoopInMode:(NSString*)runloopMode
 {
-    NSPredicate *isFinished = [NSPredicate predicateWithFormat:@"isFinished = NO AND isCancelled = NO"];
-    while ([[[self operations] filteredArrayUsingPredicate:isFinished] count] > 0) {
+    NSPredicate *executingOperations = [NSPredicate predicateWithFormat:
+                                        @"isExecuting = YES OR (isCancelled = NO AND isFinished = NO)"];
+    while ([[[self operations] filteredArrayUsingPredicate:executingOperations] count] > 0) {
         /* FXME: runloop needs signal */
         [[NSRunLoop currentRunLoop] runMode:runloopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
     }
