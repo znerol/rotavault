80e822b007501d02e5d4de00988981ad733f6823
Lorenz Schori <lo@znerol.ch>
2010-09-13 07:34:24 +0200

    implement waitUntilAllOperationsAreFinishedPollingRunLoopInMode using KVO and a dummy handler

--- src/NSOperationQueue+NonBlockingWaitUntilFinished.h
+++ src/NSOperationQueue+NonBlockingWaitUntilFinished.h
@@ -10,5 +10,27 @@
 
 
 @interface NSOperationQueue (NonBlockingWaitUntilFinished)
+/**
+ *
+ *  Wait until all operations in the queue have finished ore have been cancelled.
+ *  
+ *  isReady isExecuting isFinished isCancelled  Action
+ *    0         0           0         0         wait
+ *    0         0           0         1         don't wait
+ *    0         0           1         0         don't wait
+ *    0         0           1         1         don't wait
+ *    0         1           0         0         wait
+ *    0         1           0         1         wait
+ *    0         1           1         0         wait (inconsistent)
+ *    0         1           1         1         wait (inconsistent)
+ *    1         0           0         0         wait
+ *    1         0           0         1         don't wait
+ *    1         0           1         0         wait (inconsistent)
+ *    1         0           1         1         wait (inconsistent)
+ *    1         1           0         0         wait (inconsistent)
+ *    1         1           0         1         wait (inconsistent)
+ *    1         1           1         0         wait (inconsistent)
+ *    1         1           1         1         wait (inconsistent)
+ */
 -(void)waitUntilAllOperationsAreFinishedPollingRunLoopInMode:(NSString*)runloopMode;
 @end
--- src/NSOperationQueue+NonBlockingWaitUntilFinished.m
+++ src/NSOperationQueue+NonBlockingWaitUntilFinished.m
@@ -8,29 +8,67 @@
 
 #import "NSOperationQueue+NonBlockingWaitUntilFinished.h"
 
+@interface LCSNonBlockingWaitUntilFinishedDummyKVO : NSObject
+@end
+
+@implementation LCSNonBlockingWaitUntilFinishedDummyKVO
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    /* noop */
+}
+@end
+
 
 @implementation NSOperationQueue (NonBlockingWaitUntilFinished)
-/**
- *
- *  Wait for until all operations in the queue have finished ore have been cancelled.
- *  
- *  isReady isExecuting isFinished isCanceled Action
- *    0         0           0         0       wait
- *    0         0           0         1       don't wait
- *    0         0           1         0       don't wait
- *    0         0           1         1       don't wait
- *    0         1           0         0       wait
- *    0         1           0         1       wait
- *    1         0           0         0       wait
- *    1         0           0         1       don't wait
- */
 -(void)waitUntilAllOperationsAreFinishedPollingRunLoopInMode:(NSString*)runloopMode
 {
-    NSPredicate *executingOperations = [NSPredicate predicateWithFormat:
-                                        @"isExecuting = YES OR (isCancelled = NO AND isFinished = NO)"];
-    while ([[[self operations] filteredArrayUsingPredicate:executingOperations] count] > 0) {
-        /* FXME: runloop needs signal */
-        [[NSRunLoop currentRunLoop] runMode:runloopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+
+    static NSString *filter = @"NOT ("
+        @"(isReady = NO AND isExecuting = NO AND (isFinished = YES OR isCancelled = YES)) OR "
+        @"(isReady = YES AND isExecuting = NO AND isFinished = NO AND isCancelled = YES))";
+    
+    
+    LCSNonBlockingWaitUntilFinishedDummyKVO *dummy = [[LCSNonBlockingWaitUntilFinishedDummyKVO alloc] init];
+    NSPredicate *executingOperations = [NSPredicate predicateWithFormat:filter];
+    NSMutableSet *hasObservers = [NSMutableSet set];
+    
+    while ([[self.operations filteredArrayUsingPredicate:executingOperations] count] > 0) {
+        /*
+         * Observe relevant attributes of each operation in order to trigger a runloop-exit whenever one of those
+         * change.
+         */
+        
+        for (NSOperation *op in self.operations) {
+            if ([hasObservers containsObject:op]) {
+                [op removeObserver:dummy forKeyPath:@"isReady"];
+                [op removeObserver:dummy forKeyPath:@"isExecuting"];            
+                [op removeObserver:dummy forKeyPath:@"isCancelled"];
+                [op removeObserver:dummy forKeyPath:@"isFinished"];
+            }
+            [op addObserver:dummy forKeyPath:@"isReady" options:0 context:nil];
+            [op addObserver:dummy forKeyPath:@"isExecuting" options:0 context:nil];
+            [op addObserver:dummy forKeyPath:@"isCancelled" options:0 context:nil];
+            [op addObserver:dummy forKeyPath:@"isFinished" options:0 context:nil];
+            [hasObservers addObject:op];
+        }
+        
+        /*
+         * Wait until some observation triggers the dummy handler
+         */
+        [[NSRunLoop currentRunLoop] runMode:runloopMode beforeDate:[NSDate distantFuture]];
     }
+    
+    /* cleanup */
+    for (NSOperation *op in self.operations) {
+        if ([hasObservers containsObject:op]) {
+            [op removeObserver:dummy forKeyPath:@"isReady"];
+            [op removeObserver:dummy forKeyPath:@"isExecuting"];            
+            [op removeObserver:dummy forKeyPath:@"isCancelled"];
+            [op removeObserver:dummy forKeyPath:@"isFinished"];
+        }
+        [hasObservers removeObject:op];
+    }
+    [hasObservers removeAllObjects];
+    [dummy release];
 }    
 @end
