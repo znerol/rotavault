93081d5e919368978654d7aac9e607801d6736aa
Lorenz Schori <lo@znerol.ch>
2010-09-14 16:17:18 +0200

    add proper synchronization in waitUntilAllOperationsAreFinishedPollingRunLoopInMode

--- src/NSOperationQueue+NonBlockingWaitUntilFinished.m
+++ src/NSOperationQueue+NonBlockingWaitUntilFinished.m
@@ -10,18 +10,37 @@
 #import "LCSInitMacros.h"
 
 @interface LCSNonBlockingWaitUntilFinishedDummyKVO : NSObject {
-    NSThread *target;
+    NSThread *targetThread;
+    BOOL done;
+    BOOL postedDone;
 }
+@property(assign,readonly) BOOL done;
 @end
 
 @implementation LCSNonBlockingWaitUntilFinishedDummyKVO
 -(id)init
 {
     LCSINIT_SUPER_OR_RETURN_NIL();
-    target = [NSThread currentThread];
-    LCSINIT_RELEASE_AND_RETURN_IF_NIL(target);
+    targetThread = [NSThread currentThread];
+    LCSINIT_RELEASE_AND_RETURN_IF_NIL(targetThread);
     return self;
 }
+
+-(void)setDone
+{
+    done = YES;
+}
+
+-(void)signalDone
+{
+    if (postedDone) {
+        return;
+    }
+    [self performSelector:@selector(setDone) onThread:targetThread withObject:nil waitUntilDone:NO];
+    postedDone = YES;
+}
+
+@synthesize done;
 
 -(void)wakeRunloop
 {
@@ -30,7 +49,10 @@
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-    [self performSelector:@selector(wakeRunloop) onThread:target withObject:nil waitUntilDone:YES];
+    
+    @synchronized(self) {
+        [self performSelector:@selector(wakeRunloop) onThread:targetThread withObject:nil waitUntilDone:NO];
+    }
 }
 @end
 
@@ -38,44 +60,41 @@
 @implementation NSOperationQueue (NonBlockingWaitUntilFinished)
 -(void)waitUntilAllOperationsAreFinishedPollingRunLoopInMode:(NSString*)runloopMode
 {
-
-    static NSString *filter = @"NOT ("
-        @"(isReady = NO AND isExecuting = NO AND (isFinished = YES OR isCancelled = YES)) OR "
-        @"(isReady = YES AND isExecuting = NO AND isFinished = NO AND isCancelled = YES))";
-    
+    static NSString *filter =
+        @"(isExecuting = YES AND isFinished = NO) OR (isExecuting = NO AND isFinished = NO AND isCancelled = NO)";
+    NSPredicate *executingOperations = [NSPredicate predicateWithFormat:filter];
+        
+    /* track changes to our operations array */
     LCSNonBlockingWaitUntilFinishedDummyKVO *dummy = [[LCSNonBlockingWaitUntilFinishedDummyKVO alloc] init];
     
-    /* FIXME: we definitely have a race condition around here */
-    usleep(100000);
-
-    NSPredicate *executingOperations = [NSPredicate predicateWithFormat:filter];
-    NSMutableSet *installedObserverss = [NSMutableSet set];
-    
-    while ([[self.operations filteredArrayUsingPredicate:executingOperations] count] > 0) {
-        /*
-         * Observe relevant attributes of each operation in order to trigger a runloop-exit whenever one of those
-         * change.
-         */
-
-        for (NSOperation *op in installedObserverss) {
-            [op removeObserver:dummy forKeyPath:@"isReady"];
-            [op removeObserver:dummy forKeyPath:@"isExecuting"];            
-            [op removeObserver:dummy forKeyPath:@"isCancelled"];
-            [op removeObserver:dummy forKeyPath:@"isFinished"];
-        }
-        
-        for (NSOperation *op in self.operations) {
-            if ([installedObserverss containsObject:op]) {
-                [op removeObserver:dummy forKeyPath:@"isReady"];
-                [op removeObserver:dummy forKeyPath:@"isExecuting"];            
-                [op removeObserver:dummy forKeyPath:@"isCancelled"];
-                [op removeObserver:dummy forKeyPath:@"isFinished"];
+    NSMutableSet *observedOperations = [NSMutableSet set];
+  
+    while (!dummy.done) {
+        NSArray *runningOperations;
+        @synchronized(dummy) {
+            runningOperations = [self.operations filteredArrayUsingPredicate:executingOperations];
+            
+            if ([runningOperations count] == 0) {
+                [dummy signalDone];
             }
-            [op addObserver:dummy forKeyPath:@"isReady" options:0 context:nil];
-            [op addObserver:dummy forKeyPath:@"isExecuting" options:0 context:nil];
-            [op addObserver:dummy forKeyPath:@"isCancelled" options:0 context:nil];
-            [op addObserver:dummy forKeyPath:@"isFinished" options:0 context:nil];
-            [installedObserverss addObject:op];
+            else {
+                /* check for new operations in the queue */
+                NSMutableSet *newOperations = [NSMutableSet setWithArray:runningOperations];
+                [newOperations minusSet:observedOperations];
+                
+                /*
+                 * Observe relevant attributes of each operation in order to trigger a runloop-exit whenever one of those
+                 * change.
+                 */
+                for (NSOperation *op in newOperations) {
+                    [op addObserver:dummy forKeyPath:@"isReady" options:0 context:nil];
+                    [op addObserver:dummy forKeyPath:@"isExecuting" options:0 context:nil];
+                    [op addObserver:dummy forKeyPath:@"isCancelled" options:0 context:nil];
+                    [op addObserver:dummy forKeyPath:@"isFinished" options:0 context:nil];
+                }
+                
+                [observedOperations unionSet:newOperations];
+            }
         }
         
         /*
@@ -83,15 +102,16 @@
          */
         [[NSRunLoop currentRunLoop] runMode:runloopMode beforeDate:[NSDate distantFuture]];
     }
-    
-    /* cleanup */
-    for (NSOperation *op in installedObserverss) {
-        [op removeObserver:dummy forKeyPath:@"isReady"];
-        [op removeObserver:dummy forKeyPath:@"isExecuting"];            
-        [op removeObserver:dummy forKeyPath:@"isCancelled"];
-        [op removeObserver:dummy forKeyPath:@"isFinished"];
+
+    @synchronized(dummy) {
+        for (NSOperation *op in observedOperations) {
+            [op removeObserver:dummy forKeyPath:@"isReady"];
+            [op removeObserver:dummy forKeyPath:@"isExecuting"];            
+            [op removeObserver:dummy forKeyPath:@"isCancelled"];
+            [op removeObserver:dummy forKeyPath:@"isFinished"];
+        }
     }
-    [installedObserverss removeAllObjects];
+    
     [dummy release];
 }    
 @end
