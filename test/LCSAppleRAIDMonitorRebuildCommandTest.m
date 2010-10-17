51a3f40727d85146ae186f1e16870dd601a37c1e
Lorenz Schori <lo@znerol.ch>
2010-10-17 16:01:23 +0200

    Simplify testMonitorRebuild

--- test/LCSAppleRAIDMonitorRebuildCommandTest.m
+++ test/LCSAppleRAIDMonitorRebuildCommandTest.m
@@ -29,72 +29,60 @@
 - (void)taskInitNotification:(NSNotification*)ntf
 {
     LCSExternalCommand* cmd = [ntf object];
-    NSArray* fixture = [taskFixtureEnumerator nextObject];
+    NSData* fixture = [taskFixtureEnumerator nextObject];
     
     if(fixture == nil) {
         GHFail(@"Ran out of fixtures for test");
     }
     
-    id taskMock = [OCMockObject mockTask:cmd.task
-                   withTerminationStatus:[[fixture objectAtIndex:0] intValue]
-                              stdoutData:[fixture objectAtIndex:1]
-                              stderrData:[fixture objectAtIndex:2]];
+    id taskMock = [OCMockObject mockTask:cmd.task withTerminationStatus:0 stdoutData:fixture stderrData:[NSData data]];
     cmd.task = taskMock;
     [taskMocks addObject:taskMock];
+}
+
+- (void)setUp
+{
+    taskMocks = [[NSMutableArray alloc] init];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(taskInitNotification:)
+                                                 name:LCSTestExternalCommandTaskInitNotification
+                                               object:nil];
+}
+
+- (void)tearDown
+{
+    [taskMocks release];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:LCSTestExternalCommandTaskInitNotification
+                                                  object:nil];    
 }
 
 -(void) testMonitorRebuild
 {
     /* Build up the array of fixture-arrays to use. */
-    NSArray *taskFixtures = [[NSArray alloc] initWithObjects:
-                             [NSArray arrayWithObjects:
-                              [NSNumber numberWithInt:0],
-                              [NSData dataWithContentsOfFile:
-                               [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-degraded-rebuilding-1"
-                                                               ofType:@"txt"]],
-                              [NSData data],
-                              nil],
-                             [NSArray arrayWithObjects:
-                              [NSNumber numberWithInt:0],
-                              [NSData dataWithContentsOfFile:
-                               [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-degraded-rebuilding-2"
-                                                               ofType:@"txt"]],
-                              [NSData data],
-                              nil],
-                             [NSArray arrayWithObjects:
-                              [NSNumber numberWithInt:0],
-                              [NSData dataWithContentsOfFile:
-                               [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-degraded-rebuilding-3"
-                                                               ofType:@"txt"]],
-                              [NSData data],
-                              nil],
-                             [NSArray arrayWithObjects:
-                              [NSNumber numberWithInt:0],
-                              [NSData dataWithContentsOfFile:
-                               [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-ok" ofType:@"txt"]],
-                              [NSData data],
-                              nil],
+    NSArray *taskFixtures = [[NSArray alloc]
+                             initWithObjects:
+                             [NSData dataWithContentsOfFile:
+                              [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-degraded-rebuilding-1"
+                                                              ofType:@"txt"]],
+                             [NSData dataWithContentsOfFile:
+                              [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-degraded-rebuilding-2"
+                                                              ofType:@"txt"]],
+                             [NSData dataWithContentsOfFile:
+                              [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-degraded-rebuilding-3"
+                                                              ofType:@"txt"]],
+                             [NSData dataWithContentsOfFile:
+                              [[NSBundle mainBundle] pathForResource:@"diskutil-appleraid-list-ok" ofType:@"txt"]],
                              nil];
-    taskFixtureEnumerator = [[taskFixtures objectEnumerator] retain];
-    taskMocks = [[NSMutableArray alloc] init];
-                                 
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(taskInitNotification:)
-                                                 name:LCSTestExternalCommandTaskInitNotification
-                                               object:nil];
     
+    taskFixtureEnumerator = [[taskFixtures objectEnumerator] retain];
+                                 
     LCSAppleRAIDMonitorRebuildCommand *cmd = [LCSAppleRAIDMonitorRebuildCommand
                                               commandWithRaidUUID:@"76898140-1ED1-41AB-931F-2E30D015829F"
                                               devicePath:@"/dev/disk3s1"];
     cmd.updateInterval = 0;
     LCSCommandController *ctl = [[LCSCommandController controllerWithCommand:cmd] retain];
-    
-    id ctlMock = [OCMockObject mockForProtocol:@protocol(LCSTestNotificationConsumer)];
-    [[ctlMock expect] consumeNotification:[OCMArg any]];
-    [[NSNotificationCenter defaultCenter] addObserver:ctlMock
-                                             selector:@selector(consumeNotification:)
-                                                 name:[LCSCommandController notificationNameStateEntered:LCSCommandStateFinished]
-                                               object:ctl];
     
     [ctl start];
     [ctl waitUntilDone];
@@ -102,17 +90,10 @@
     for (id taskMock in taskMocks) {
         [taskMock verify];
     }
-    [ctlMock verify];
     
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:LCSTestExternalCommandTaskInitNotification
-                                                  object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:ctlMock
-                                                    name:[LCSCommandController notificationNameStateEntered:LCSCommandStateFinished]
-                                                  object:ctl];
+    GHAssertEquals(ctl.exitState, LCSCommandStateFinished, @"Expected LCSCommandStateFinished");
     
     [ctl release];
-    [taskMocks release];
     [taskFixtures release];
     [taskFixtureEnumerator release];
 }
